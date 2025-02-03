e3441316b428d350ae9b06c437d05ff86c5ab0c2
Stephanie <stephnjonjo@gmail.com>
2025-02-03 16:51:22 +0300

    updated my codes

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 function ContactForm() {
   const [contacts, setContacts] = useState([]);
@@ -122,7 +120,6 @@ function ContactForm() {
       <ul>
         {contacts.map((contact) => (
           <li key={contact.id}>
-            <FontAwesomeIcon icon={faUser} size="2x" style={{ marginRight: '10px' }} />
             {contact.name} ({contact.phone}, {contact.email}, {contact.address}){' '}
             <button onClick={() => handleEdit(contact)}>Edit</button>{' '}
             <button onClick={() => handleDelete(contact.id)}>Delete</button>
